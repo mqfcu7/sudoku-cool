b7a4f35d662a6d9ad1c4dce8ba3ecf945ab7c300
mqfcu7 <mqfcu7@gmail.com>
2018-06-06 20:34:11 +0800

    init project

---
+++ app/src/main/java/com/mqfcu7/jiangmeilan/sudokucool/BoardView.java
@@ -0,0 +1,12 @@
+package com.mqfcu7.jiangmeilan.sudokucool;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BoardView extends View {
+    public BoardView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/mqfcu7/jiangmeilan/sudokucool/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mqfcu7.jiangmeilan.sudokucool;
+
+import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
